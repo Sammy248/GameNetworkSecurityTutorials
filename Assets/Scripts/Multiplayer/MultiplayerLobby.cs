42713fb07a6557113be359af0848013d586a561e
Sammy <samtchaplin248@gmail.com>
2025-10-14 17:28:14 +0100

    first half of last part of tutorial

---
+++ Assets/Scripts/Multiplayer/MultiplayerLobby.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Multiplayer : MonoBehaviourPunCallbacks
+{
+    public Transform LoginPanel;
+    public Transform SelectionPanel;
+    public Transform CreateRoomPanel;
+    public Transform InsideRoomPanel;
+    public Transform ListRoomsPanel;
+
+    public InputField roomNameInput;
+
+    public void CreateARoom()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 4;
+        roomOptions.IsVisible = true;
+
+        PhotonNetwork.CreateRoom(roomNameInput.text, roomOptions);
+    }
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Room has been created");
+    }
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+
+    }
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Room has been Joined");
+        ActivatePanel("InsideRoom");
+    }
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+    public override void OnLeftRoom()
+    {
+        Debug.Log("Room has been left");
+        ActivatePanel("CreateRoom");
+
+    }
+    public void LoginButtonClicked()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        ActivatePanel("Selection");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("We have connected to the master server");
+    }
+
+    public void ActivatePanel(string panelName)
+    {
+        LoginPanel.gameObject.SetActive(false);
+        SelectionPanel.gameObject.SetActive(false);
+        CreateRoomPanel.gameObject.SetActive(false);
+        InsideRoomPanel.gameObject.SetActive(false);
+        ListRoomsPanel.gameObject.SetActive(false);
+
+        if(panelName == LoginPanel.gameObject.name)
+        {
+            LoginPanel.gameObject.SetActive(true);
+        }
+        else if (panelName == SelectionPanel.gameObject.name)
+        {
+            SelectionPanel.gameObject.SetActive(true);
+        }
+        else if (panelName == CreateRoomPanel.gameObject.name)
+        {
+            CreateRoomPanel.gameObject.SetActive(true);
+        }
+        else if (panelName == InsideRoomPanel.gameObject.name)
+        {
+            InsideRoomPanel.gameObject.SetActive(true);
+        }
+        else if (panelName == ListRoomsPanel.gameObject.name)
+        {
+            ListRoomsPanel.gameObject.SetActive(true);
+        }
+    }
+
+    public void DisconnectButtonClicked()
+    {
+        Debug.Log("Disconnected from Master Server");
+        PhotonNetwork.Disconnect();
+    }
+
+
+}
